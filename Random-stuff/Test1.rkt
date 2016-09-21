c60f7f6d5499d993ff54f57ba7c26b50e1ff558c
Leroxit <leroxit@ukr.net>
2016-09-21 21:16:24 +0300

    Formula implementation
    S = 1 + 1/2! + 1/3! + ... + 1/n!

---
+++ Random-stuff/Test1.rkt
@@ -0,0 +1,12 @@
+#lang sicp
+;S = 1 + 1/2! + 1/3! + ... + 1/n!
+(define (sum n)
+  (define (fac x)
+  (if (= x 1) 1
+      (* x (fac (- x 1)))))
+  (define (sump n r i)
+  (if (= i n) r
+      (sump n (+ r (/ 1 (fac (+ i 1)))) (+ i 1))))
+  (if (< n 1) 0  
+  (sump n 1 1)))
+  
