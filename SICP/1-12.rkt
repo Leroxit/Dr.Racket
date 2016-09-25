950bd017d461eecc0952ef9005877bde57f4dfcb
Leroxit <leroxit@ukr.net>
2016-09-25 19:20:59 +0300

    Update 1-12.rkt

--- SICP/1-12.rkt
+++ SICP/1-12.rkt
@@ -1,6 +1,6 @@
 #lang sicp
- (define (pascal row col) 
-   (cond ((or (< row col) (< col 0)) 0) 
-         ((or (= row col) (= col 1)) 1) 
-         (+ (pascal (- row 1) col)
-            (pascal (- row 1) (- col -1)))))
+ (define (pas x y)
+   (cond ((or (< x 1) (< y 1)) 0)
+         ((or (= x 1) (= x y)) 1)
+         (else (+ (pas x (- y 1))
+                  (pas (- x 1) (- y 1))))))
