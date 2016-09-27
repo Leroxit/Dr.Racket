79321bd0ebb3f018696f418b3eeb455f5caeeb45
Leroxit <leroxit@ukr.net>
2016-09-27 16:50:55 +0300

    Update 1-16.rkt

--- SICP/1-16.rkt
+++ SICP/1-16.rkt
@@ -1,7 +1,8 @@
 #lang sicp
 (define (square x)
   (* x x))
-
+  
+;Using formula, that (b^n/2)^2 = (b^2)^n/2, so when exp is even - square the number, and divide exp. by 2
 (define (exp-iter b n)
   (define (iter b n a)
   (cond ((= n 0) a)
