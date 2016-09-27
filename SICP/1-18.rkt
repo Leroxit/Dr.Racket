00b0ca94a5df4d2563888f673b6ba353a2208f0a
Leroxit <leroxit@ukr.net>
2016-09-27 17:02:34 +0300

    Update 1-18.rkt

--- SICP/1-18.rkt
+++ SICP/1-18.rkt
@@ -4,8 +4,8 @@
 (define (halve x)
   (/ x 2))
 (define (mult x y)
-  (define (iter x y a)
-  (cond ((= y 0) a)
-        ((= (remainder y 2) 0) (iter (double x) (halve y) a)) 
-        (else (iter x (- y 1) (+ a x)))))
+  (define (iter x y a)                      ; a - accumulator
+  (cond ((= y 0) a)                         ; termination
+        ((= (remainder y 2) 0) (iter (double x) (halve y) a))  ; if "y" is "even", then x*y= (2\2)*(x*y) = 2*x*(y\2)
+        (else (iter x (- y 1) (+ a x)))))                      ; x*y=x*(y-1)+x
   (iter x y 0))
