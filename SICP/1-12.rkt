4bae47cfa8eca93566a3b65d372e33aa9a9cf6ae
Leroxit <leroxit@ukr.net>
2016-09-27 17:03:40 +0300

    Update 1-12.rkt

--- SICP/1-12.rkt
+++ SICP/1-12.rkt
@@ -2,5 +2,5 @@
  (define (pas x y) 
    (cond ((or (< x 1) (< y 1)) 0)
          ((or (= x 1) (= x y)) 1)    ;First number in a row and the last = 1 (By defenition: numbers of collumns = number of rows, so x=y)
-         (else (+ (pas x (- y 1))    ;Add two numbers, which are higher (y-1), one is same x-coordinate, and the other is previous (x-1
+         (else (+ (pas x (- y 1))    ;Add two numbers, which are higher (y-1), one is same x-coordinate, and the other is previous (x-1)
                   (pas (- x 1) (- y 1))))))
