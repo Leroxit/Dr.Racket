70e970b8dcea5a81c2810fefb9fde554134345a0
Leroxit <leroxit@ukr.net>
2016-09-27 19:39:57 +0300

    Update and rename 1-22.rkt to 1-22,23.rkt

--- SICP/1-22,23.rkt
+++ SICP/1-22,23.rkt
@@ -8,10 +8,12 @@
 
 (define (smallest-divisor n)
   (find-divisor n 2))
+(define (next x)   ;Exercise 1,23
+  (if (= x 2) 3 (+ x 2)))
 (define (find-divisor n test-divisor)
   (cond ((> (square test-divisor) n) n)
         ((divides? test-divisor n) test-divisor)
-        (else (find-divisor n (+ test-divisor 1)))))
+        (else (find-divisor n (next test-divisor 1)))))
 (define (divides? a b)
   (= (remainder b a) 0))
 
