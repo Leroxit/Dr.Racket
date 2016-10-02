029bbaa9c81289763e663b32ce3444781fdf21bc
Leroxit <leroxit@ukr.net>
2016-10-02 22:48:23 +0300

    Add files via upload

---
+++ SICP/1-31.rkt
@@ -0,0 +1,27 @@
+#lang sicp
+(define (product term a next b)
+  (if (> a b)
+      1
+      (* (term a)
+         (product term (next a) next b))))
+
+(define (product-it term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (* result (term a)))))
+  (iter a 1))
+
+(define (inc n) (+ n 1))
+
+(define (factorial x)
+  (define (identity x) x)
+  (product-it identity 1 inc x))
+
+(define (pi a)
+  (define (top x)               ;Using this procedures for this formula's sequences. For numerators (2,4,4,6,6..) if counter is not even -> (-1)
+    (if (even? x) x (- x 1)))   ;For denominator: If counter is even -> (-1).  (Example for numerator: 3->2, 4->4, 5->4, 6->6, 7->6...)
+  (define (bot  x)
+    (if (even? x) (- x 1) x))
+  (* 4 (/ (product-it top 3 inc a)
+          (product-it bot 3 inc a))))
