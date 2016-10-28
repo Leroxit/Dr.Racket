a4e94a64022484228aa3e97e251f25377a1af808
Leroxit <leroxit@ukr.net>
2016-10-28 10:52:15 +0300

    Add files via upload

---
+++ SICP/2-21.rkt
@@ -0,0 +1,15 @@
+#lang sicp
+(define (map proc items)
+  (if (null? items)
+      nil
+      (cons (proc (car items))
+            (map proc (cdr items)))))
+
+(define (square-list items)
+  (if (null? items)
+      nil
+      (cons (* (car items) (car items))
+            (square-list (cdr items)))))
+
+(define (square-list1 items)
+  (map (lambda (x) (* x x)) items))
