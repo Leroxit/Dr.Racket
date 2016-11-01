21310ffc737c717b8c1adac6e13d3d0ae9d047bc
Leroxit <leroxit@ukr.net>
2016-11-02 00:01:10 +0200

    Add files via upload

---
+++ SICP/2-31.rkt
@@ -0,0 +1,10 @@
+#lang racket
+(define (square-tree tree)
+  (tree-map square tree))
+(define (square x) (* x x))
+
+(define (tree-map proc tree)
+  (cond ((null? tree) null)
+        ((not (pair? tree)) (proc tree))
+        (else (cons (tree-map proc (car tree))
+                    (tree-map proc (cdr tree))))))
