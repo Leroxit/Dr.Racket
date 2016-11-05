f1e23db2b3ca9486239b645786b7d1e756c7ccfc
Leroxit <leroxit@ukr.net>
2016-11-05 22:52:24 +0200

    Update 2-32.rkt

--- SICP/2-32.rkt
+++ SICP/2-32.rkt
@@ -4,5 +4,5 @@
       (list null)
       (let ((rest (subsets (cdr s))))
         (append rest (map (lambda (x) (cons (car s) x)) rest)))))
-        
+;Description of this algorithm is here:        
 ;https://en.wikipedia.org/wiki/Power_set#Algorithms
