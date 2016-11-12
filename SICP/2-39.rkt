c25fb32df71df61c00a01158b90384a3302ca31e
Leroxit <leroxit@ukr.net>
2016-11-12 22:39:57 +0200

    Add files via upload

---
+++ SICP/2-39.rkt
@@ -0,0 +1,19 @@
+#lang racket
+(define (fold-left op initial sequence)
+  (define (iter result rest)
+    (if (null? rest)
+        result
+        (iter (op result (car rest))
+              (cdr rest))))
+  (iter initial sequence))
+
+(define (fold-right op initial sequence)
+   (if (null? sequence)
+       initial
+       (op (car sequence)
+           (fold-right op initial (cdr sequence)))))
+
+(define (reverser sequence)
+  (fold-right (lambda (x y) (append y (list x))) null sequence))
+(define (reversel sequence)
+  (fold-left (lambda (x y) (cons y x)) null sequence))
