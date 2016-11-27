08bcc59a8ed83db7180641cd3ff184585f15744b
Leroxit <leroxit@ukr.net>
2016-11-28 00:01:36 +0200

    Add files via upload

---
+++ SICP/2-45.rkt
@@ -0,0 +1,12 @@
+#lang racket
+(require sicp-pict)
+
+(define (split direction method)
+  (lambda (painter n)
+    (if (= n 0)
+        painter
+        (let ((smaller ((split direction method) painter (- n 1))))
+          (direction painter (method smaller smaller))))))
+
+(define right-split (split beside below))
+(define up-split (split below beside))
