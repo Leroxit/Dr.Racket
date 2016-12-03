d443ed6fa3f79411b42ab7b20bb306ba4006f029
Leroxit <leroxit@ukr.net>
2016-12-03 23:29:47 +0200

    Update 2-48.rkt

--- SICP/2-48.rkt
+++ SICP/2-48.rkt
@@ -1,9 +1,7 @@
 #lang racket
 (define (make-segment vector1 vector2)
   (cons vector1 vector2))
-
 (define (start-segment segment)
   (car segment))
-
 (define (end-segment segment)
   (cdr segment))
