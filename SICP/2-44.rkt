b87c1b1447a8754d26005de75363a5001be28893
Leroxit <leroxit@ukr.net>
2016-12-26 23:13:34 +0200

    Update 2-44.rkt

--- SICP/2-44.rkt
+++ SICP/2-44.rkt
@@ -6,7 +6,6 @@
       painter
       (let ((smaller (right-split painter (- n 1))))
         (beside painter (below smaller smaller)))))
-
 (define (up-split painter n)
   (if (= n 0)
       painter
