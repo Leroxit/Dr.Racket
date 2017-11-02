3941ab07dd48e9665091f9e4aede8de2f39f86c9
Leroxit <leroxit@ukr.net>
2017-11-02 02:35:36 +0200

    Update 1-11.rkt

--- SICP/1-11.rkt
+++ SICP/1-11.rkt
@@ -1,12 +1,16 @@
-(define (f n)
-   (if (< n 3)
-       n
-       (f-iter 2 1 0 n)))
+#lang sicp
+(define (f-rec n)
+  (if (< n 3)
+        n
+        (+ (f-rec (- n 1))
+           (* 2 (f-rec (- n 2)))
+           (* 3 (f-rec (- n 3))))))
 
-(define (f-iter a b c count)
-   (if (< count 3)
-       a
-       (f-iter (+ a (* 2 b) (* 3 c))
-               a
-               b
-               (- count 1))))
+(define (f-it n)
+  (define (f-count a b c i)
+    (if (= i n)
+        a
+        (f-count (+ a (* 2 b) (* 3 c)) a b (+ i 1))))
+  (if (< n 3)
+      n
+      (f-count 2 1 0 2)))
