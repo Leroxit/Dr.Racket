e4f863b5e3a64c8bb5402a665b4343bd89ada6d3
Leroxit <leroxit@ukr.net>
2018-05-08 13:42:16 +0300

    Update 1-12.rkt

--- SICP/1-12.rkt
+++ SICP/1-12.rkt
@@ -1,6 +1,10 @@
 #lang sicp
- (define (pas x y) 
-   (cond ((or (< x 1) (< y 1)) 0)
-         ((or (= x 1) (= x y)) 1)    ;First number in a row and the last = 1 (By defenition: numbers of collumns = number of rows, so x=y)
-         (else (+ (pas x (- y 1))    ;Add two numbers, which are higher (y-1), one is same x-coordinate, and the other is previous (x-1)
-                  (pas (- x 1) (- y 1))))))
+(define (pascal x y)
+  (define (pas x y)
+    (if (or (= y 1) (= x y))         ;First number in a row and the last = 1 (By defenition: numbers of collumns = number of rows, so x=y)
+        1
+        (+ (pas (- x 1) y)           ;Add two numbers, which are higher (x-1), one is same y-coordinate, and the other is previous (y-1)    
+           (pas (- x 1) (- y 1)))))
+  
+  (if (or (< x 1) (< y 1)) 0         ;Check for "less then 1" collumns or rows
+      (pas x y)))
